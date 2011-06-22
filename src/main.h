d8c6644a4a92c355e1f84febc1e26550e37dc3d4
Fergus Noble <fergusnoble@gmail.com>
2011-06-22 16:31:21 -0700

    Default to timer off.

--- src/main.h
+++ src/main.h
@@ -33,7 +33,7 @@
 
 // If TIMER is enabled then the bootloader will jump to user code after
 // a period of time if there has been no activity on the USB interface.
-#define TIMER
+//#define TIMER
 // TIMER_TIMEOUT sets the period of the timer timeout in units of
 // approximately 43.7 milliseconds.
 #define TIMER_TIMEOUT 229 // 10s timeout
